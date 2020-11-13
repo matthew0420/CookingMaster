3695afdafcd1cd8b897a77c9e35c69ba19a80d5e
Matthew Gigante <mgiga0420@gmail.com>
2020-11-13 13:23:15 -0500

    Added scene components and player movement
    Player script takes input from wasd or arrow keys depending on which player is being controlled, and the basic components of the scene have been placed.

---
+++ Cooking_Master/Assets/_Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerScript : MonoBehaviour
+{
+    public bool isPlayer1;
+
+    public float speed = 2f;
+
+    Rigidbody2D rigidBody;
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 input = Vector3.zero;
+        if (isPlayer1)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+        } else
+        {
+            input.x = Input.GetAxisRaw("Horizontal2");
+            input.y = Input.GetAxisRaw("Vertical2");
+        }
+
+        Vector3 direction = input.normalized;
+
+        Vector3 movement = direction * speed * Time.fixedDeltaTime;
+
+        rigidBody.MovePosition(transform.position + movement);
+    }
+}
