ddb0adb95334c60aa1671a8583ef67267381d48c
Matthew Gigante <mgiga0420@gmail.com>
2020-11-15 13:14:52 -0500

    Added Pick up functionality to player characters
    When the WASD player presses left shift when in range of an ingredient spot, if they have the inventory space they will pick up the ingredient and said ingredient will appear above the player's head. The same goes for the arrow key player when pressing right shift.

---
+++ Cooking_Master/Assets/_Scripts/FoodScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodScript : MonoBehaviour
+{
+    void Update()
+    {
+        this.gameObject.transform.position = this.gameObject.transform.parent.gameObject.transform.position;
+    }
+}
--- Cooking_Master/Assets/_Scripts/PlayerScript.cs
+++ Cooking_Master/Assets/_Scripts/PlayerScript.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerScript : MonoBehaviour
 {
-    public bool isPlayer1;
-
     public float speed = 2f;
+    public List<GameObject> Inventory;
+    public List<GameObject> FoodItems;
+
+    public bool isPlayer1;
 
     Rigidbody2D rigidBody;
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
     }
+
     void FixedUpdate()
     {
         Vector3 input = Vector3.zero;
@@ -29,9 +32,40 @@ public class PlayerScript : MonoBehaviour
         }
 
         Vector3 direction = input.normalized;
-
         Vector3 movement = direction * speed * Time.fixedDeltaTime;
-
         rigidBody.MovePosition(transform.position + movement);
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Inventory.Count < 2)
+        {
+            if (collision.gameObject.tag == "FoodItem" && Input.GetKeyDown(KeyCode.LeftShift) && isPlayer1)
+            {
+                Debug.Log("pressing!");
+                InstantiateFoodItem(collision.gameObject.name);
+            }
+            else if (collision.gameObject.tag == "FoodItem" && Input.GetKeyDown(KeyCode.RightShift) && !isPlayer1)
+            {
+                InstantiateFoodItem(collision.gameObject.name);
+            }
+        }
+    }
+
+    private void InstantiateFoodItem(string FoodToSpawn)
+    {
+        for (int i = 0; i < FoodItems.Count; i++)
+        {
+            if (FoodItems[i].name == FoodToSpawn)
+            {
+                var myItemToInstantiate = Instantiate(FoodItems[i].gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity); ;
+                myItemToInstantiate.transform.parent = gameObject.transform.GetChild(Inventory.Count).gameObject.transform;
+                myItemToInstantiate.transform.position = new Vector3(0, 0, 0);
+                myItemToInstantiate.transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+                Inventory.Add(myItemToInstantiate);
+                break;
+            }
+        }
+    }
 }
+
